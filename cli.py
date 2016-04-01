13b90d4a95c881cdab4f5101a2548b9da38d52f7
Mads Nielsen <k4emic@gmail.com>
2016-04-01 21:08:49 +0200

    pip installer

---
+++ cli.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import click
+
+
+@click.command()
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='magerestore',
+    version='0.1',
+    py_modules=['cli'],
+    install_requires=[
+        'Click>=6'
+    ],
+    entry_points='''
+        [console_scripts]
+        magerestore=cli:main
+    ''',
+)
