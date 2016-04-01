8a1192bf20dc2d79e4433571f9878ab1e82d5c38
Mads Nielsen <k4emic@gmail.com>
2016-04-01 23:24:11 +0200

    Add test helper for accessing static files

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,11 +3,15 @@ import os
 from magerestore.config import Config
 
 
+def get_static_file_path(filename):
+    return os.path.join(os.path.dirname(__file__), 'static', filename)
+
+
 class ConfigTest(unittest.TestCase):
 
     def test_from_json(self):
         config = Config()
-        config.from_json(os.path.join(os.path.dirname(__file__), 'static', 'config.json'))
+        config.from_json(get_static_file_path('config.json'))
 
         self.assertTrue('sample' in config.repos, 'sample repo is seen in repo config')
         self.assertEqual(len(config.repos), 1, 'only one sample repo')
