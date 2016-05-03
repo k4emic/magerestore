381c2f2355c37cd796b252a1dc3de809c060af3b
Mads Nielsen <k4emic@gmail.com>
2016-05-03 21:07:08 +0200

    Update setup.py for pypi distribution

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 
+long_description = """Magerestore is a tool to automate fetching backups from a remote server and unpacking them on a magento installation.
+
+Database import and media folder unzipping is the ony currently supported operations.
+
+Magerestore uses n98-magerun to handle some tasks, but are not required for all operations."""
+
 setup(
     name='magerestore',
     version='0.1',
+    description="Tool for restoring magento install from predefined backups",
+    long_description=long_description,
+    url="https://github.com/k4emic/magerestore",
+    license="GPLv3",
+    author="Mads Nielsen",
     py_modules=['cli'],
     install_requires=[
         'Click>=6', 'paramiko>=1.16'
